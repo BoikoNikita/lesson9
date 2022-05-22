593be896744f2b9bdfd99390c7e033bfdf73fa90
«Nikita_Boiko» <nikita-bojko@yandex.ru>
2022-05-22 10:57:09 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+void Rec (int n)
+{
+    if(n<m)
+    return;
+    Console.Write(n + " ");
+    Rec(n-1);
+}
+Rec(n);
