3fff41eacbc1ea9ff0a01a7dafbed214bdc90afc
«Nikita_Boiko» <nikita-bojko@yandex.ru>
2022-05-22 12:16:40 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int Ack(int m, int n) 
+{
+    if(m == 0) return n + 1;
+    if(n == 0) return Ack(m - 1, 1); 
+    return Ack(m - 1, Ack(m, n - 1));
+}
+Console.WriteLine($"Значение функции Аккермана: A({m}, {n}): {Ack(m, n)}.");
